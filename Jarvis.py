2e42174969874439eb3d49e868ae19398b0d0dc1
Om <shelkeom67@gmail.com>
2025-11-18 21:34:07 +0530

    Starts with speech recognition and text to speech

--- Jarvis.py
+++ Jarvis.py
@@ -1,1 +1,22 @@
-print("Hello Starting jarvis")
+import speech_recognition as sr
+import pyttsx3 as ts
+
+r=sr.Recognizer()
+engine=ts.init()
+
+def speak(t):
+    engine.say(t)
+    engine.runAndWait()
+
+with sr.Microphone() as source:
+    print("Talk")
+    print("Say Something")
+    audio=r.listen(source)
+    try:
+        text=r.recognize_google(audio)
+        print("You said:",text)
+        speak("You said:"+text)
+
+    except:
+        print("Sorry I did not get it")
+        speak("Sorry,I did not get it")
