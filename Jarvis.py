ad04b594b70b7d38ac8ade6f68eca9bb94010b90
Om <shelkeom67@gmail.com>
2025-11-21 13:23:50 +0530

    Improves the listening time of Jarvis

--- Jarvis.py
+++ Jarvis.py
@@ -4,11 +4,12 @@ import webbrowser
 from datetime import datetime
 
 r = sr.Recognizer()
+r.pause_threshold=0.8
 
 
 
 def speak(t):
-    engine = ts.init()
+    engine=ts.init()
     engine.setProperty('rate',150)
     engine.say(t)
     engine.runAndWait()
@@ -16,48 +17,49 @@ def speak(t):
 
 current_hour =datetime.now().hour
 
-if 5<=current_hour<=12:
+if 5<=current_hour<12:
     speak("Good Morning  I am your personal assistant jarvis  what can I do for you")
-elif 12<=current_hour<=17:
+elif 12<=current_hour<17:
     speak("Good Afternoon  I am your personal assistant jarvis what can I do for you")
-elif 17<=current_hour<=21:
+elif 17<=current_hour<21:
     speak("Good Evening I am your personal assistant jarvis what can I do for you")
 else:
     speak("Good Night")
 
-
-while True:
-    with sr.Microphone() as source:
+with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source,duration=1)
-        print("Talk")
-        print("Say Something")
-        audio = r.listen(source,phrase_time_limit=20)
-        try:
-            text = r.recognize_google(audio).lower()
-            print("You said:", text)
-            speak("You said: " + text)
 
-            if text == "open geeks for geeks website":
-                webbrowser.open("https://www.geeksforgeeks.org")
-                continue
+        while True:
+            print("Talk")
+            print("Say Something")
+            audio = r.listen(source,phrase_time_limit=5)
+            try:
+                text = r.recognize_google(audio).lower()
+                print("You said:", text)
+                speak("You said: " + text)
 
-            if text == "open youtube":
-                webbrowser.open_new("https://www.youtube.com/")
-                continue
+                if "open geeks for geeks" in text:
+                    webbrowser.open("https://www.geeksforgeeks.org")
+                    continue
 
-            if text == "open my github account":
-                webbrowser.open_new("https://github.com/")
-                continue
+                if "open youtube" in text:
+                    webbrowser.open_new("https://www.youtube.com/")
+                    continue
 
-            if text=="open chatgpt":
-                webbrowser.open_new("https://chatgpt.com/")
-                continue
+                if "open github" in text:
+                    webbrowser.open_new("https://github.com/")
+                    continue
 
-            if text == "exit now":
-                break
+                if "open chatgpt" in text:
+                    webbrowser.open_new("https://chatgpt.com/")
+                    continue
 
-        except:
-            print("Sorry I did not get it")
-            speak("Sorry, I did not get it")
+                if "exit now" in text:
+                    speak("Thanks for using me")
+                    break
 
-speak("Thanks for using this program")
+            except sr.UnknownValueError:
+                pass
+            except sr.WaitTimeoutError:
+                pass
+
